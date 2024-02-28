731ce539501b43bdc2745206734c0149771ac52b
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-02-29 08:19:46 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,15 +1,74 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] citations) {
-        Arrays.sort(citations);
+   public int solution(int[] citations) {
+       int answer = 0;
+       int length = citations.length;
+       citations = mergeSort(citations, 0, length - 1);
+       
+       int index = citations.length - 1;
+       while(answer != length) {
+       	if(citations[index] >= length) {
+       		answer++;
+       		index--;
+       		continue;
+       	} else {
+       		answer = 0;
+       		length--;
+       		index = citations.length - 1;
+       	}
+       }
+       
+       return answer;
+   }
 
-        int max = 0;
-        for(int i = citations.length-1; i > -1; i--){
-            int min = (int)Math.min(citations[i], citations.length - i);
-            if(max < min) max = min;
-        }
+	private int[] mergeSort(int[] citations, int i, int j) {
+		if(i < j) {
+			int mid = (i + j) / 2;
+			
+			mergeSort(citations, i, mid);
+			mergeSort(citations, mid + 1, j);
+			merge(citations, i, mid, j);
+		}
+		return citations;
+	}
 
-        return max;
-    }
+	private void merge(int[] citations, int start, int mid, int end) {
+		int leftSize = mid - start + 1;
+		int rightSize = end - mid;
+		
+		int[] leftArray = new int[leftSize];
+		int[] rightArray = new int[rightSize];
+		
+		for (int i = 0; i < leftArray.length; i++) {
+			leftArray[i] = citations[start + i];
+		}
+		for (int i = 0; i < rightArray.length; i++) {
+			rightArray[i] = citations[mid + 1 + i];
+		}
+		
+		int s = 0, e = 0, k = start;
+		while(s < leftSize
+				&& e < rightSize) {
+			if(leftArray[s] <= rightArray[e]) {
+				citations[k] = leftArray[s];
+				s++;
+			} else {
+				citations[k] = rightArray[e];
+				e++;
+			}
+			k++;
+		}
+		
+		while(s < leftSize) {
+			citations[k] = leftArray[s];
+			s++;
+			k++;
+		}
+		while(e < rightSize) {
+			citations[k] = rightArray[e];
+			e++;
+			k++;
+		}
+	}
 }
