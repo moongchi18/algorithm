64486f5722eed09921ff94da6dda50373167cca2
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-03-08 08:18:57 +0900

    [level 2] Title: 기능개발, Time: 1.95 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] workDay = new int[progresses.length];
+        for (int i = 0; i < workDay.length; i++) {
+        	if((100 - progresses[i]) % speeds[i] == 0) {
+        		workDay[i] = (100 - progresses[i]) / speeds[i];
+        	} else {
+        		workDay[i] = ((100 - progresses[i]) / speeds[i]) + 1;
+        	}
+		}
+        
+        int index = 0;
+        List<Integer> list = new ArrayList<>();
+        while(index < progresses.length) {
+        	int count = 1;
+        	for (int i = index + 1; i < workDay.length; i++) {
+				if(workDay[index] >= workDay[i]) {
+					count++;
+				}else {
+					break;
+				}
+			}
+        	index += count;
+        	
+        	list.add(count);
+        }
+        
+        return list.stream()
+        			.mapToInt(Integer::intValue)
+        			.toArray();
+    }
+}
