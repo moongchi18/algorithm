88f75f390db009ed594fbc20baecebd4184f34b6
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-03-07 19:59:45 +0900

    [level 2] Title: 튜플, Time: 8.99 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+        Queue<Integer> que = new LinkedList<>();
+        
+		String element = s.replaceAll("\\{\\{", "")
+						.replaceAll("\\}\\}", "");
+		String[] elements = element.split("\\},\\{");
+		int[][] array = Arrays.stream(elements)
+							.map(str -> str.split(","))
+							.map(temp -> Arrays.stream(temp)
+													.mapToInt(Integer::parseInt)
+													.toArray())
+							.sorted(Comparator.comparingInt(intArr -> intArr.length))
+							.toArray(int[][]::new);
+
+		for (int[] is : array) {
+			List<Integer> temp = Arrays.stream(is)
+										.boxed()
+										.collect(Collectors.toList());
+			for (int q : que) {
+				int index = temp.indexOf(q);
+				if(index != -1) {
+					temp.remove(index);
+				}
+			}
+			
+			for (int t : temp) {
+				que.add(t);
+			}
+		}
+		
+		
+		
+        return que.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
