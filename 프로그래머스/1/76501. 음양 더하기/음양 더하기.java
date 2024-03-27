ea758f7d09d099e1b1356d82ea18e611ef54465a
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-03-28 08:08:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,27 +1,8 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for (int i = 0; i < signs.length; i++) {
-        	Abs abs = new Abs(absolutes[i], signs[i]);
-			answer += abs.abs();
-		}
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
         return answer;
-    }
-    
-    static class Abs {
-    	private int num;
-    	private boolean isPlus;
-    	
-    	public Abs(int v, boolean b) {
-    		this.num = v;
-    		this.isPlus = b;
-    	}
-    	
-    	public int abs() {
-        	if(!isPlus) {
-        		num = num * (-1);
-        	}
-        	return num;
-        }
     }
 }
