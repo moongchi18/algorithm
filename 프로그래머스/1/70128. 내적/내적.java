7ea90ff6efbcc559514bc5820a6e3f4970a0f0ab
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-04-03 08:15:36 +0900

    [level 1] Title: 내적, Time: 1.97 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,10 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
-        for (int i = 0; i < b.length; i++) {
-			answer += a[i] * b[i];
-		}
-        
-        return answer;
+        return IntStream.range(0, a.length)
+        		.map(index -> a[index] * b[index])
+        		.sum();
     }
 }
