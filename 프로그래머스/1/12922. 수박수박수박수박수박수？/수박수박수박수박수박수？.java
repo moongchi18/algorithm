74bde540b02c6a6d143db3649477c33c25e002e5
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-04-04 08:09:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.03 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,10 +1,14 @@
 class Solution {
     public String solution(int n) {
-    	char[] ref = new char[] {'수','박'};
-    	char[] result = new char[n];
-    	for (int i = 0; i < result.length; i++) {
-			result[i] = ref[i % 2];
-		}
-    	return String.valueOf(result);
+        String su="";
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                su = su+"수";
+            }
+            else if(i%2!=0){
+                su = su+"박";
+            }
+        }
+        return su;
     }
 }
