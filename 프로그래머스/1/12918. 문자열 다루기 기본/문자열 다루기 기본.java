38b388a2d8f8e3b3f936ad2c3d98b0aa941b2702
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-04-08 08:47:00 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.18 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        return s.matches("[0-9]{4}") || s.matches("[0-9]{6}");
+        return s.matches("^[0-9]{4}$|^[0-9]{6}$");
     }
 }
