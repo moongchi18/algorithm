9c532b12e0226a420bef248306e40e8be062ec59
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-04-22 08:32:07 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.00 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,17 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 1; i <= food.length - 1; i++) {
-			int count = food[i] / 2;
-			if(count > 0) {
-				char[] arr = new char[count];
-				Arrays.fill(arr, Character.forDigit(i, 10));
-				String val = String.valueOf(arr);
-				sb.append(val);
-			}
-		}
-        return sb.toString() + "0" + sb.reverse().toString();
+        StringBuilder builder = new StringBuilder();
+        for (int i=1; i<food.length; i++) {
+            int result = food[i] / 2;
+            builder.append(String.valueOf(i).repeat(result));
+        }
+        String answer = builder + "0";
+        return answer + builder.reverse();
     }
 }
