39fc3465927c4cc17296c21464cc08580e4be1bc
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-04-24 08:16:35 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.36 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,19 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-    	PriorityQueue<Integer> que = new PriorityQueue<>((Integer o1, Integer o2) -> {
-    		return o1.compareTo(o2);
-    	});
+    	PriorityQueue<Integer> que = new PriorityQueue<>();
     	int[] result = new int[score.length];
     	
     	for (int i = 0; i < score.length; i++) {
 			que.add(score[i]);
-			int min = Integer.MAX_VALUE;
 			if(que.size() > k) {
 				que.poll();
 			} 
-			min = que.peek();
-			result[i] = min;
+			result[i] = que.peek();
 		}
     	
     	
