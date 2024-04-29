1ff2b6aa45615ae014cf273b4475926e0690162b
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-04-30 07:48:02 +0900

    [level 1] Title: 소수 찾기, Time: 310.48 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+        	int count = 0;
+        	double sqrt = Math.sqrt(i);
+        	if(sqrt % 1 == 0) {
+        		continue;
+        	}
+			for (int j = 1; j <= sqrt; j++) {
+				if(i % j == 0) {
+					if(i / j == j) {
+						count++;
+						break;
+					} else {
+						count++;
+					}
+					if(count > 1) {
+						break;
+					}
+				}
+			}
+			if(count == 1) {
+				answer++;
+			}
+		}
+        return answer;
+    }
+}
