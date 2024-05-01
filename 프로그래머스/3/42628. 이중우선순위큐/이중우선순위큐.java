5ee027f9c59525b02f02484b6416f6e651164aa4
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-05-02 08:02:18 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.43 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,28 +2,28 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
-    public int[] solution(String[] operations) {
-    	PriorityQueue<Integer> que = new PriorityQueue<>();
-    	PriorityQueue<Integer> reverseQue = new PriorityQueue<>(Comparator.reverseOrder());
-    	
-    	for (String op : operations) {
-			if(op.startsWith("I")) {
+	public int[] solution(String[] operations) {
+		PriorityQueue<Integer> ascQue = new PriorityQueue<>();
+		PriorityQueue<Integer> descQue = new PriorityQueue<>(Comparator.reverseOrder());
+
+		for (String op : operations) {
+			if (op.startsWith("I")) {
 				int num = Integer.parseInt(op.substring(2));
-				que.add(num);
-				reverseQue.add(num);
+				ascQue.add(num);
+				descQue.add(num);
 			} else {
-				if(op.equals("D 1")) {
-					que.remove(reverseQue.poll());
-				} else {
-					reverseQue.remove(que.poll());
+				if (op.equals("D 1")) {
+					ascQue.remove(descQue.poll());
+				} else if (op.equals("D -1")) {
+					descQue.remove(ascQue.poll());
 				}
 			}
 		}
-    	
-    	if(que.size() > 1) {
-    		return new int[] {reverseQue.poll(), que.poll()};
-    	} else {
-    		return new int[] {0, 0};
-    	}
-    }
+		int[] result = new int[] { 0, 0 };
+		if (ascQue.size() > 1) {
+			result[0] = descQue.poll();
+			result[1] = ascQue.poll();
+		}
+		return result;
+	}
 }
