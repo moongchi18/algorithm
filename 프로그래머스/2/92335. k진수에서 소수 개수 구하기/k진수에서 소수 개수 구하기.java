376b5ce4c3b5a5d1ba6fa60e5258d961d5a23632
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-05-08 07:49:54 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,37 +1,24 @@
 class Solution {
 	public int solution(int n, int k) {
 		int answer = 0;
-		String str = decimalToBase(n, k);
+		String str = Integer.toString(n, k);
 
 		String[] arr = str.split("0");
 		
-		for (String s : arr) {
+		Target : for (String s : arr) {
 			if(s.isBlank()
 					|| s.equals("1")) {
 				continue;
 			}
 			long num = Long.parseLong(s);
 			
-			int modCount = 0;
-			for (int i = 1; i <= Math.sqrt(num); i++) {
+			for (int i = 2; i <= Math.sqrt(num); i++) {
 				if (num % i == 0) {
-					modCount++;
+					continue Target;
 				}
 			}
-			if (modCount == 1) {
-				answer++;
-			}
+			answer++;
 		}
 		return answer;
-	}
-
-	public static String decimalToBase(int decimal, int mod) {
-		StringBuilder result = new StringBuilder();
-		while (decimal > 0) {
-			result.insert(0, decimal % mod);
-			decimal /= mod; // 몫을 새로운 값으로 갱신
-		}
-
-		return result.toString();
 	}
 }
