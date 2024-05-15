976ca4100e8631d44a2cddc6770385fb50c04421
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2024-05-16 07:47:45 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.82 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,23 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int t, int m, int p) {
+    	long startTime = System.currentTimeMillis();
         StringBuilder result = new StringBuilder();
-        Queue<Character> que = new LinkedList<>();
         int num = 0;
-        for (int i = 0; i < t; i++) {
-			for (int j = 1; j <= m; j++) {
-				if (que.isEmpty()) {
-					String numString = Integer.toString(num++, n).toUpperCase();
-					for (int k = 0; k < numString.length(); k++) {
-						que.add(numString.charAt(k));
-					}
-				}
-				char temp2 = que.poll();
-				if(j == p) {
-					result.append(temp2);
+        int index = 0;
+        int order = p - 1;
+        for (int i = 0; i < t * m; ) {
+			String numStr = Integer.toString(num++, n).toUpperCase();
+			for (int j = 0; j < numStr.length(); j++) {
+				if(index < t * m
+						&& index++ % m == order) {
+					result.append(numStr.charAt(j));
 				}
 			}
+			i += numStr.length();
 		}
+        
+        long endTime = System.currentTimeMillis();
+        System.out.printf("### 실행시간 : %,d ms %n", (endTime - startTime));
         return result.toString();
     }
 }
